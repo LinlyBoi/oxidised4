39ea77120ffd8d02fdca1064ccd7c01ed0989a35
LinlyBoi <libkyy@e.email>
2023-04-29 19:33:30 +0300

    unimplemented shit

---
+++ src/gamedata/score_checkers.rs
@@ -0,0 +1,12 @@
+use array2d::Array2D;
+
+use super::Disk;
+
+pub fn one_direction(board: &Array2D<Disk>, index: &(usize, usize)) -> i32 {
+    let current_disk = board.get(index.0, index.1);
+    unimplemented!()
+}
+pub fn two_direction(board: &Array2D<Disk>, index: &(usize, usize)) -> i32 {
+    let current_disk = board.get(index.0, index.1);
+    unimplemented!()
+}
