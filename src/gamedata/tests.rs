3d3d6fb3f9baa802a64944e20782644797365514
LinlyBoi <libkyy@e.email>
2023-05-02 17:35:03 +0300

    all tests passed :D

--- src/gamedata/tests.rs
+++ src/gamedata/tests.rs
@@ -56,8 +56,8 @@ fn scan_updown() {
     board.play(Disk::BLU, 0);
     board.play(Disk::BLU, 0);
     board.play(Disk::BLU, 0);
-    assert_eq!(3, scan(&board.columns, &(4, 0), Direction::DOWN, 3));
-    assert_eq!(3, scan(&board.columns, &(3, 0), Direction::DOWN, 3));
+    assert_eq!(4, scan(&board.columns, &(4, 0), Direction::DOWN, 4));
+    assert_eq!(4, scan(&board.columns, &(3, 0), Direction::DOWN, 4));
 }
 #[test]
 fn scan_updown2() {
@@ -66,7 +66,7 @@ fn scan_updown2() {
     board.play(Disk::RED, 0);
     board.play(Disk::BLU, 0);
     board.play(Disk::BLU, 0);
-    assert_eq!(0, scan(&board.columns, &(0, 0), Direction::UP, 3));
+    assert_eq!(1, scan(&board.columns, &(0, 0), Direction::UP, 4));
 }
 #[test]
 fn scan_forwardback() {
@@ -76,9 +76,8 @@ fn scan_forwardback() {
     board.play(Disk::BLU, 2);
     board.play(Disk::BLU, 3);
 
-    assert!(!matches!(Disk::RED, Disk::BLU));
-    assert_eq!(1, scan(&board.columns, &(0, 0), Direction::RIGHT, 3));
-    assert_eq!(1, scan(&board.columns, &(0, 3), Direction::LEFT, 3));
+    assert_eq!(4, scan(&board.columns, &(0, 0), Direction::RIGHT, 4));
+    assert_eq!(4, scan(&board.columns, &(0, 3), Direction::LEFT, 4));
 }
 #[test]
 fn scan_forwardback2() {
@@ -87,8 +86,8 @@ fn scan_forwardback2() {
     board.play(Disk::BLU, 1);
     board.play(Disk::RED, 2);
     board.play(Disk::BLU, 3);
-    assert_eq!(0, scan(&board.columns, &(0, 0), Direction::RIGHT, 3));
-    assert_eq!(0, scan(&board.columns, &(0, 3), Direction::LEFT, 3));
+    assert_eq!(2, scan(&board.columns, &(0, 0), Direction::RIGHT, 4));
+    assert_eq!(1, scan(&board.columns, &(0, 3), Direction::LEFT, 4));
 }
 #[test]
 fn scan_diag1() {
@@ -103,8 +102,8 @@ fn scan_diag1() {
     board.play(Disk::RED, 3);
     board.play(Disk::RED, 3);
     board.play(Disk::BLU, 3);
-    assert_eq!(1, scan(&board.columns, &(0, 0), Direction::UPRIGHT, 3));
-    assert_eq!(1, scan(&board.columns, &(3, 3), Direction::DOWNLEFT, 3));
+    assert_eq!(4, scan(&board.columns, &(0, 0), Direction::UPRIGHT, 4));
+    assert_eq!(4, scan(&board.columns, &(3, 3), Direction::DOWNLEFT, 4));
 }
 #[test]
 fn scan_diag2() {
@@ -120,8 +119,8 @@ fn scan_diag2() {
     board.play(Disk::RED, 0);
     board.play(Disk::BLU, 0);
     dbg!(&board.columns.as_columns());
-    assert_eq!(1, scan(&board.columns, &(0, 3), Direction::UPLEFT, 3));
-    assert_eq!(1, scan(&board.columns, &(3, 0), Direction::DOWNRIGHT, 3));
+    assert_eq!(4, scan(&board.columns, &(0, 3), Direction::UPLEFT, 4));
+    assert_eq!(4, scan(&board.columns, &(3, 0), Direction::DOWNRIGHT, 4));
 }
 #[test]
 fn variant_eq_test() {
