f23e147a201400a0781e21effd6b2299d39f1802
LinlyBoi <libkyy@e.email>
2023-05-05 15:45:58 +0300

    skeleton for minimax (pls help)

---
+++ src/gamedata/algorithms.rs
@@ -0,0 +1,10 @@
+use super::{heuristic, Board, Disk};
+pub fn minimax_decision(board: &Board, depth: i32) -> Board {
+    todo!()
+}
+fn maximise(board: &Board, depth: &i32) -> (Board, i32) {
+    todo!()
+}
+fn minimise(board: &Board, depth: &i32) -> (Board, i32) {
+    todo!()
+}
