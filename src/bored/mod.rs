5bebf782eb28607fe38c232570a1958a15366b57
LinlyBoi <libkyy@e.email>
2023-05-06 17:50:59 +0300

    play state and menu state are separate now

--- src/bored/mod.rs
+++ src/bored/mod.rs
@@ -1,14 +1,16 @@
+use raylib::prelude::Color;
+
 use crate::gamedata::{Board, Disk};
 
 #[cfg(test)]
 mod tests;
-pub struct GameState {
+pub struct PlayState {
     pub circles: Vec<(i32, i32, Disk)>,
     pub bottom: Vec<i32>,
     pub player_turn: bool,
     pub board: Board,
 }
-impl Default for GameState {
+impl Default for PlayState {
     fn default() -> Self {
         Self {
             circles: vec![],
@@ -17,4 +19,22 @@ impl Default for GameState {
             board: Board::default(),
         }
     }
+}
+pub struct MenuState {
+    difficulty: i32,
+    p1: (Color, Disk),
+    p2: (Color, Disk),
+}
+impl Default for MenuState {
+    fn default() -> Self {
+        Self {
+            difficulty: 3,
+            p1: (Color::RED, Disk::P1),
+            p2: (Color::YELLOW, Disk::P2),
+        }
+    }
+}
+pub enum GameState {
+    Play(PlayState),
+    MainMenu(MenuState),
 }
