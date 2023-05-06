080452137efe21e1b0a2105eb5683e842ade39c3
LinlyBoi <libkyy@e.email>
2023-05-06 12:24:29 +0300

    game state struct

--- src/bored/mod.rs
+++ src/bored/mod.rs
@@ -1,2 +1,20 @@
+use crate::gamedata::{Board, Disk};
+
 #[cfg(test)]
 mod tests;
+pub struct GameState {
+    circles: Vec<(i32, i32, Disk)>,
+    empty: Vec<i32>,
+    player_turn: bool,
+    board: Board,
+}
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            circles: vec![],
+            empty: vec![],
+            player_turn: false,
+            board: Board::default(),
+        }
+    }
+}
